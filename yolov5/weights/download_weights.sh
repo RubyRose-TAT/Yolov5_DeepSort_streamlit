7f600fa873b0da314b0a9900ecc787fd7ad5ad3b
RubyRose-TAT <rubyrosetat@gmail.com>
2022-06-14 19:31:44 +0800

    first commit

--- yolov5/weights/download_weights.sh
+++ yolov5/weights/download_weights.sh
@@ -1,10 +1,3 @@
-#!/bin/bash
-# Download common models
-
-python -c "
-from utils.google_utils import *;
-attempt_download('weights/yolov5s.pt');
-attempt_download('weights/yolov5m.pt');
-attempt_download('weights/yolov5l.pt');
-attempt_download('weights/yolov5x.pt')
-"
+version https://git-lfs.github.com/spec/v1
+oid sha256:e1d085a46598c4910355279342c34a92bf7a173a5ebb207860af01c215c3176f
+size 245
